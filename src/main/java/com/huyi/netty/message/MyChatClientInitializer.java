fd1f90fc85b3e8e2865bab232cc0c9b7d5674be6
“huyi <1007885927@qq.com>
2019-09-05 22:43:00 +0800

    ChatDemo

--- src/main/java/com/huyi/netty/message/MyChatClientInitializer.java
+++ src/main/java/com/huyi/netty/message/MyChatClientInitializer.java
@@ -1,25 +1,23 @@
-package com.huyi.netty.socket;
+package com.huyi.netty.message;
 
-import io.netty.channel.Channel;
+import com.huyi.netty.socket.MyClientHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.codec.Delimiters;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
-import io.netty.handler.codec.LengthFieldPrepender;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.util.CharsetUtil;
 
-public class MyClientInitializer extends ChannelInitializer<SocketChannel>{
-
+public class MyChatClientInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
         ChannelPipeline pipeline =ch.pipeline();
         pipeline.addLast(new DelimiterBasedFrameDecoder(4906, Delimiters.lineDelimiter()));
         pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
         pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
-        pipeline.addLast(new MyClientHandler());
+        pipeline.addLast(new MyChatServerHandler());
     }
 }
