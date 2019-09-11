15fed0040dc54889e3e1bf67ad4d40df4fa17c04
“huyi <1007885927@qq.com>
2019-09-11 20:31:43 +0800

    NIODemo

--- src/main/java/com/huyi/nio/NioTest1.java
+++ src/main/java/com/huyi/nio/NioTest1.java
@@ -7,18 +7,17 @@ public class NioTest1 {
 
     public static void main(String[] args) {
         IntBuffer  buffer = IntBuffer.allocate(10);
-        System.out.println(buffer.capacity());
+        System.out.println("capacity:" +buffer.capacity());
         for (int i=0;i<buffer.capacity();i++){
             int randomNumber =new SecureRandom().nextInt(20);
             buffer.put(randomNumber);
         }
+
         //状态的翻转
         buffer.flip();
+
         while (buffer.hasRemaining()){
             System.out.println(buffer.get());
         }
-
-
-
     }
 }
